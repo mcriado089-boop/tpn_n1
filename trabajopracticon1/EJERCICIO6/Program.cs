8416ed0a6bc242d18ebca822d8c0abca7df09d9f
mcriado089-boop <mcriado089@gmail.com>
2026-08-20 18:37:22 -0300

    inicio trabajopractico N1
    se resolvió lo ejercicio 5 y 6

---
+++ trabajopracticon1/EJERCICIO6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double PRECIOORIGINAL = 0;
+            int DESCUENTO = 0;
+            double PORCENTAJEDESCUENTO = 0;
+            double Preciofinal = 0;
+
+            Console.WriteLine("ingrse el precio del producto");
+            PRECIOORIGINAL = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el porcentje del descuento");
+            PORCENTAJEDESCUENTO= int.Parse(Console.ReadLine());
+            // ACA CALCULO CUANTO SERA EL DESCUENTO EN PESOS
+           DESCUENTO = PRECIOORIGINAL * (PORCENTAJEDESCUENTO/ 100);
+            // CALCULA EL PRECIO FINAL QUE DEVO PAQGAR, AQPLICANDO EL DESCUENTO
+
+            Preciofinal = PRECIOORIGINAL - DESCUENTO;
+            Console.WriteLine($"PRECIO ORIGINAL:$ {PRECIOORIGINAL}");
+
+
+        }
+    }
+}
---
+++ trabajopracticon1/ejercicio5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadhora = 0;
+            double preciodehora = 0;
+
+            double resultado =0;
+
+            Console.WriteLine("ingresa la cantidad de horas trabajadas");
+            cantidadhora = int.Parse ( Console. ReadLine());
+
+            Console.WriteLine("ingrese el precio de la hora");
+            preciodehora= double.Parse(Console.ReadLine());
+
+            resultado = cantidadhora * preciodehora;
+
+            Console.WriteLine($" el total a pagar es:${resultado.ToString("F2")}");
+
+
+        }
+    }
+}
