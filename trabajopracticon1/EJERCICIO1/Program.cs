510a0cd6535a447284b7b398a2f283d7c9da4b34
mcriado089-boop <mcriado089@gmail.com>
2026-08-28 18:00:41 -0300

    ejercicio1
    trabajo terminado

---
+++ trabajopracticon1/EJERCICIO1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*CREAR UN PROGRAMAEscribe un programa que solicite al usuario ingresar el precio de un producto y la cantidad que desea 
+            comprar.Calcula el costo total de la compra y muestra el resultado en pantalla.*/
+
+
+            /* el programa lo hize para quea libre producto y el usuario ponga cualquier producto */
+
+
+
+            double precio = 0;
+
+            string producto = "0";
+
+            int cantidad = 0;
+
+            double total = 0;
+
+            
+
+            Console.WriteLine("ELIJA UN PRODUCTO");
+            producto = Console.ReadLine();
+
+            Console.WriteLine("precio del producto");
+             precio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("elija cantidad");
+           cantidad =int.Parse(Console.ReadLine());
+
+            total = precio * cantidad;
+            Console.WriteLine($"su tutal es:${total}");
+
+
+        }
+    }
+}
