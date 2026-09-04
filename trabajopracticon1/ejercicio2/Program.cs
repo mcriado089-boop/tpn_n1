f62a2efcb2512bf473abdf841760e9e13c314faa
mcriado089-boop <mcriado089@gmail.com>
2026-09-04 01:58:45 -0300

    TP1EJE2

---
+++ trabajopracticon1/ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir que el usuaria ingre 2 medidas//
+            //largo , ancho y de pór rsultado el perimetro del terreno rectangular//
+
+            double largo = 0;
+            double ancho = 0;
+            double  perimetro = 0;
+
+            Console.WriteLine("escriba el largo del terreno");
+            largo= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el ancho del terreno");
+            ancho= double.Parse(Console.ReadLine());
+
+            perimetro= largo + ancho;
+            Console.WriteLine($"su perimetro total es :{perimetro}");
+
+        }
+    }
+}
---
+++ trabajopracticon1/ejercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribe un programa que solicite al usuario ingresar la distancia recorrida (en kilómetros) y el tiempo 
+                         empleado(en horas).Calcula la velocidad promedio utilizando la fórmula: 
+                                              Velocidad = distancia / tiempo.
+                                    Muestra el resultado en pantalla*/
+
+            double kilometros = 0;
+            double horas = 0;
+            double velocidad = 0;
+
+            Console.WriteLine("ingrese la distancia recorrida");
+            kilometros =double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el tiempo que empleo en el viaje ");
+            horas =double.Parse(Console.ReadLine());
+
+            velocidad =kilometros / horas;
+            Console.WriteLine($"SU VELOCIDAD FUE DE{velocidad}");
+
+
+
+
+
+
+        }
+    }
+}
